813bf7cfb6253810e778d36ea2ddb01be3312ca6
marco091 <marco091@users.noreply.github.com>
2017-01-12 20:36:02 -0800

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunToAutoTest.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by marco on 1/12/2017.
+ */
+
+public class RunToAutoTest extends LinearOpMode {
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    public void runOpMode(){
+        leftMotor = hardwareMap.dcMotor.get("motor2");
+        rightMotor = hardwareMap.dcMotor.get("motor1");
+
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        moveForward(300,1);
+        move(-150,150,-1,1);
+    }
+
+    public void moveForward(int pos, double power){
+        leftMotor.setTargetPosition(leftMotor.getCurrentPosition() + pos);
+        rightMotor.setTargetPosition(rightMotor.getCurrentPosition() + pos);
+
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
+
+        while(leftMotor.isBusy() && rightMotor.isBusy() && opModeIsActive()){
+            telemetry.addData("leftMotorPos",leftMotor.getCurrentPosition());
+            telemetry.addData("rightMotorPos",rightMotor.getCurrentPosition());
+            telemetry.update();
+        }
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+
+    }
+
+    public void move(int pos1, int pos2, double power1, double power2){
+        leftMotor.setTargetPosition(leftMotor.getCurrentPosition() + pos1);
+        rightMotor.setTargetPosition(rightMotor.getCurrentPosition() + pos2);
+
+        leftMotor.setPower(power1);
+        rightMotor.setPower(power2);
+
+        while(leftMotor.isBusy() && rightMotor.isBusy() && opModeIsActive()){
+            telemetry.addData("leftMotorPos",leftMotor.getCurrentPosition());
+            telemetry.addData("rightMotorPos",rightMotor.getCurrentPosition());
+            telemetry.update();
+
+            if(!leftMotor.isBusy()){
+                leftMotor.setPower(0);
+            }
+            if(!rightMotor.isBusy()){
+                rightMotor.setPower(0);
+            }
+        }
+        //safety stop
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+
+    }
+}
